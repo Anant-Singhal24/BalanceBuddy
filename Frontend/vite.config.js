bd2c8d397a7b235fd8c0290ff86c72ba2c5baac5
Anant Singhal <anantsinghal0124@gmail.com>
2025-06-13 17:41:53 +0530

    fix the rewrite issue

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   base: './', // 👈 THIS is important
   build: {
     outDir: "dist",
     assetsDir: "assets",
